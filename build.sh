1eab38d90a48ec895ec05d0a3566f6b52552807b
Yanhui Shen <shen.elf@gmail.com>
2011-06-17 23:17:03 +0800

    use cmake to build

---
+++ build.sh
@@ -0,0 +1,3 @@
+g++ -c -x c++ head.cpp -o head.o
+g++ -c -x c++ main.cpp -o main.o
+g++ main.o head.o
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "head.h"
 using namespace std;
 
 int main()
 {
     cout << "hello world" << endl;
-    cout << "test" << endl;
+    cout << "test!" << endl;
+
+    cout << add(1, 2) << endl;
+
     return 0;
 }
